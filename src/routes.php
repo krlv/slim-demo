38ec7852365f3ebb96364bb9a0a90413232add3f
krlv <eug.krlv@gmail.com>
2016-01-07 22:29:34 +0300

    Add /api route group

--- src/routes.php
+++ src/routes.php
@@ -8,3 +8,16 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->group('/api', function () {
+    $this->get('/tasks', function ($request, $response, $args) {
+        // Sample log message
+        $this->logger->info("Slim-Skeleton '/api/tasks' route");
+
+        // TODO: fetch list of tasks
+        $tasks = [];
+
+        // Render index view
+        return $response->withJson(['tasks' => $tasks]);
+    });
+});
