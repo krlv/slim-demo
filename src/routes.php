452decca93dee222fedc7a850292b4838b82b955
krlv <eug.krlv@gmail.com>
2016-01-09 23:47:58 +0300

    Add /api/categories routes

--- src/routes.php
+++ src/routes.php
@@ -54,4 +54,49 @@ $app->group('/api', function () {
         // Return empty response with 204 No Content code
         return $response->withJson(null, 204);
     });
+
+    $this->get('/categories', function ($request, $response, $args) {
+        // Sample log message
+        $this->logger->info("Slim-Skeleton '/api/categories' route");
+
+        // TODO: fetch list of categories
+        $categories = [];
+
+        // Return response as JSON
+        return $response->withJson(['categories' => $categories]);
+    });
+
+    $this->get('/categories/{category_id}', function ($request, $response, $args) {
+        // Sample log message
+        $this->logger->info("Slim-Skeleton '/categories/{category_id}' route");
+
+        // TODO: fetch category by ID
+        $task = [
+            'id'    => $args['category_id'],
+            'title' => 'Category ' . $args['category_id'],
+        ];
+
+        // Return response as JSON
+        return $response->withJson(['task' => $task]);
+    });
+
+    $this->post('/categories', function ($request, $response, $args) {
+        // Sample log message
+        $this->logger->info("Slim-Skeleton '/api/categories' route");
+
+        // TODO: save new category
+
+        // Return empty response with 201 Created code
+        return $response->withJson(null, 201);
+    });
+
+    $this->put('/categories/{category_id}', function ($request, $response, $args) {
+        // Sample log message
+        $this->logger->info("Slim-Skeleton '/api/categories/{category_id}' route");
+
+        // TODO: update existing category
+
+        // Return empty response with 204 No Content code
+        return $response->withJson(null, 204);
+    });
 });
