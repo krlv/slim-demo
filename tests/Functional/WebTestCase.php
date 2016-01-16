eca07047a9f1abc5a5612e25671e69a261f701c3
krlv <eug.krlv@gmail.com>
2016-01-16 21:49:05 +0300

    Create a base class for functional tests

---
+++ tests/Functional/WebTestCase.php
@@ -0,0 +1,103 @@
+<?php
+namespace Skeletn\Tests\Functional;
+
+use Skeletn\App\AppFactory;
+use Slim\App;
+
+/**
+ * Base class functional tests
+ *
+ * @package Skeletn\Tests\Functional
+ */
+class WebTestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+    * Application instance
+    *
+    * @var App
+    */
+    protected $app;
+
+    /**
+     *
+     *
+     * @var WebTestClient
+     */
+    protected $client;
+
+    /**
+     * Setting up test environment
+     */
+    public static function setUpBeforeClass()
+    {
+        //TODO: init database
+    }
+
+    /**
+    * Setting up the application
+    */
+    protected function setUp()
+    {
+        $this->app = $this->createApplication();
+        //TODO: init database tables
+        $this->client = $this->createClient();
+    }
+
+    /**
+    * Creates the application
+    *
+    * @return App
+    */
+    public function createApplication()
+    {
+        return AppFactory::createTestApp(require __DIR__ . '/../../src/settings.php');
+    }
+
+    /**
+    * Creates a Client
+    *
+    * @param array $server Server parameters
+    *
+    * @return WebTestClient A Client instance
+    */
+    public function createClient(array $server = array())
+    {
+        return new WebTestClient($this->app, $server);
+    }
+
+    /**
+     * Assert that response status equals to expected code
+     *
+     * @param $code
+     */
+    public function assertStatusCode($code)
+    {
+        $this->assertEquals($code, $this->client->getStatusCode());
+    }
+
+    /**
+     * Assert that response content type equals to expected content type
+     *
+     * @param $type
+     */
+    public function assertContentType($type)
+    {
+        $this->assertContains($type, $this->client->getHeader('Content-Type'));
+    }
+
+    /**
+     * Assert that JSON response equals to expected data
+     *
+     * @param array $expected
+     * @param int $code
+     */
+    public function assertJsonResponse(array $expected, $code = 200)
+    {
+        $this->assertStatusCode($code);
+        $this->assertContentType('application/json;charset=utf-8');
+
+        $expected = json_encode($expected);
+        $actual   = $this->client->getResponse();
+        $this->assertEquals($expected, $actual);
+    }
+}
