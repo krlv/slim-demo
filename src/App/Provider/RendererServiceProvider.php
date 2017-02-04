aaa245a6aa0a0314e2d71e22c0a12ce41041d7b9
krlv <eug.krlv@gmail.com>
2017-02-04 17:46:01 +0200

    Create Renderer Service Provider

---
+++ src/App/Provider/RendererServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+namespace Skeleton\App\Provider;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+/**
+ * Slim PHP Renderer service provider
+ */
+class RendererServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $pimple)
+    {
+        $pimple['renderer'] = function (\Slim\Container $c) {
+            $settings = $c->get('settings')['renderer'];
+            return new \Slim\Views\PhpRenderer($settings['template_path']);
+        };
+    }
+}
