4e71f67e3a8d225c2f33a907aeefcc9dcb82c0b9
krlv <eug.krlv@gmail.com>
2017-02-04 17:46:56 +0200

    Create Monolog logger Service Provider

---
+++ src/App/Provider/LoggerServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+namespace Skeleton\App\Provider;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+/**
+ * Monolog logger service provider
+ */
+class LoggerServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $pimple)
+    {
+        $pimple['logger'] = function (\Slim\Container $c) {
+            $settings = $c->get('settings')['logger'];
+            $logger = new \Monolog\Logger($settings['name']);
+            $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+            $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], \Monolog\Logger::DEBUG));
+            return $logger;
+        };
+    }
+}
