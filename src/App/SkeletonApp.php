20cd7e8a1c2e59fd77e703f954d0f101dc0cf001
krlv <eug.krlv@gmail.com>
2017-02-04 17:48:26 +0200

    Introduce Service Providers to manage App dependencies

--- src/App/SkeletonApp.php
+++ src/App/SkeletonApp.php
@@ -1,15 +1,31 @@
 <?php
 namespace Skeleton\App;
 
+use Pimple\ServiceProviderInterface;
+
 class SkeletonApp extends \Slim\App
 {
+    /**
+     * Registers application dependencies
+     *
+     * @return SkeletonApp
+     */
     public function registerServices(): SkeletonApp
     {
-        // Set up dependencies
-        require __DIR__ . '/../dependencies.php';
+        $this
+            ->register(new Provider\RendererServiceProvider())
+            ->register(new Provider\DbalServiceProvider())
+            ->register(new Provider\LoggerServiceProvider())
+        ;
+
         return $this;
     }
 
+    /**
+     * Registers application middleware
+     *
+     * @return SkeletonApp
+     */
     public function registerMiddleware(): SkeletonApp
     {
         // Register middleware
@@ -17,10 +33,29 @@ class SkeletonApp extends \Slim\App
         return $this;
     }
 
+    /**
+     * Registers application routes
+     *
+     * @return SkeletonApp
+     */
     public function registerRoutes(): SkeletonApp
     {
         // Register routes
         require __DIR__ . '/../routes.php';
+        return $this;
+    }
+
+    /**
+     * Registers a service provider
+     *
+     * @param ServiceProviderInterface $provider Service provider
+     * @param array $values Array of values to configure service provider
+     *
+     * @return SkeletonApp
+     */
+    public function register(ServiceProviderInterface $provider, array $values = []): SkeletonApp
+    {
+        $this->getContainer()->register($provider, $values);
         return $this;
     }
 }
