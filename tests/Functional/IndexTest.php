f274cc028b3774cd716cc3087460f31dac66e6f9
krlv <eug.krlv@gmail.com>
2017-02-05 21:57:07 +0200

    Create functional test for index route

---
+++ tests/Functional/IndexTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Skeleton\Test\Functional;
+
+class IndexTest extends WebTestCase
+{
+    public function testIndexAction()
+    {
+        $this->client->request('GET', '/');
+        $this->assertStatusCode(200);
+        $this->assertContains('SlimFramework', $this->client->getResponse());
+    }
+
+    public function testIndexActionWithName()
+    {
+        $this->client->request('GET', '/Eugene');
+        $this->assertStatusCode(200);
+        $this->assertContains('Eugene', $this->client->getResponse());
+    }
+
+    public function testIndexActionNotAllowed()
+    {
+        $this->client->request('POST', '/Eugene');
+        $this->assertStatusCode(405);
+        $this->assertContains('Method not allowed', $this->client->getResponse());
+    }
+}
