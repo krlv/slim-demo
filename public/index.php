168f3e9f361e7320159f929a1ab1ecec2af8878a
krlv <eug.krlv@gmail.com>
2017-04-02 21:30:54 +0300

    #2 Remove session_start() from front controller

--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-session_start();
-
 // Instantiate the app
 $app = \Skeleton\App\AppFactory::createApp(require __DIR__ . '/../src/settings.php');
 
