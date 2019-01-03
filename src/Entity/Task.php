682de41faed225f2285d671a8e06a7c7904fdc0c
krlv <eug.krlv@gmail.com>
2019-01-03 04:11:21 -0800

    #15 Design proper Task entity

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -14,18 +14,60 @@ final class Task
     private $title;
 
     /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var int
+     */
+    private $priority;
+
+    /**
      * @var boolean
      */
     private $isDone;
 
     /**
-     * @param string  $title
-     * @param boolean $isDone
+     * @var boolean
      */
-    public function __construct(string $title, bool $isDone = false)
-    {
-        $this->title  = $title;
-        $this->isDone = $isDone;
+    private $isDeleted;
+
+    /**
+     * @var \DateTimeImmutable
+     */
+    private $doneAt;
+
+    /**
+     * @var \DateTimeImmutable
+     */
+    private $deletedAt;
+
+    /**
+     * @param string                  $title
+     * @param string                  $description
+     * @param int                     $priority
+     * @param bool                    $isDone
+     * @param bool                    $isDeleted
+     * @param \DateTimeImmutable|null $doneAt
+     * @param \DateTimeImmutable|null $deletedAt
+     */
+    public function __construct(
+        string $title,
+        string $description = '',
+        int $priority = 0,
+        bool $isDone = false,
+        bool $isDeleted = false,
+        \DateTimeImmutable $doneAt = null,
+        \DateTimeImmutable $deletedAt = null
+    ) {
+        $this->title       = $title;
+        $this->description = $description;
+        $this->priority    = $priority;
+        $this->isDone      = $isDone;
+        $this->isDeleted   = $isDeleted;
+        $this->doneAt      = $doneAt;
+        $this->deletedAt   = $deletedAt;
     }
 
     /**
@@ -45,6 +87,84 @@ final class Task
     }
 
     /**
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return $this
+     */
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPriority(): int
+    {
+        return $this->priority;
+    }
+
+    /**
+     * @param int $priority
+     * @return $this
+     */
+    public function setPriority(int $priority): self
+    {
+        $this->priority = $priority;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDone(): bool
+    {
+        return $this->isDone;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDeleted(): bool
+    {
+        return $this->isDeleted;
+    }
+
+    /**
+     * @return \DateTimeImmutable|null
+     */
+    public function getDoneAt(): ?\DateTimeImmutable
+    {
+        return $this->doneAt;
+    }
+
+    /**
+     * @return \DateTimeImmutable|null
+     */
+    public function getDeletedAt(): ?\DateTimeImmutable
+    {
+        return $this->deletedAt;
+    }
+
+    /**
      * Mark the task as done
      *
      * @return $this
@@ -52,6 +172,7 @@ final class Task
     public function done(): self
     {
         $this->isDone = true;
+        $this->doneAt = new \DateTimeImmutable;
         return $this;
     }
 
@@ -63,6 +184,31 @@ final class Task
     public function undone(): self
     {
         $this->isDone = false;
+        $this->doneAt = null;
+        return $this;
+    }
+
+    /**
+     * Mark the task as deleted (hide the task)
+     *
+     * @return $this
+     */
+    public function delete(): self
+    {
+        $this->isDeleted = true;
+        $this->deletedAt = new \DateTimeImmutable;
+        return $this;
+    }
+
+    /**
+     * Mark the task as un-deleted (undo the task deletion)
+     *
+     * @return $this
+     */
+    public function restore(): self
+    {
+        $this->isDeleted = false;
+        $this->deletedAt = null;
         return $this;
     }
 }
--- tests/Unit/Entity/TaskTest.php
+++ tests/Unit/Entity/TaskTest.php
@@ -11,8 +11,23 @@ class TaskTest extends TestCase
 
     public function testConstructor()
     {
-        $task = new Task($title = 'Task');
+        $task = new Task(
+            $title       = 'Task',
+            $description = 'Task Description',
+            $priority    = 1,
+            $isDone      = true,
+            $isDeleted   = true,
+            $doneAt      = new \DateTimeImmutable('+1 day'),
+            $deletedAt   = new \DateTimeImmutable('+2 day'),
+        );
+
         $this->assertSame($title, $task->getTitle());
+        $this->assertSame($description, $task->getDescription());
+        $this->assertSame($priority, $task->getPriority());
+        $this->assertSame($isDone, $task->isDone());
+        $this->assertSame($isDeleted, $task->isDeleted());
+        $this->assertSame($doneAt, $task->getDoneAt());
+        $this->assertSame($deletedAt, $task->getDeletedAt());
     }
 
     public function testGetId()
@@ -24,15 +39,52 @@ class TaskTest extends TestCase
         $this->assertEquals($id, $task->getId());
     }
 
+    public function testTitle()
+    {
+        $task = new Task('Task');
+        $task->setTitle($title = 'New Task');
+        $this->assertEquals($title, $task->getTitle());
+    }
+
+    public function testDescription()
+    {
+        $task = new Task('Task');
+        $task->setDescription($description = 'Description');
+        $this->assertEquals($description, $task->getDescription());
+    }
+
+    public function testPriority()
+    {
+        $task = new Task('Task');
+        $task->setPriority($priority = 10);
+        $this->assertEquals($priority, $task->getPriority());
+    }
+
     public function testDone()
     {
         $task = (new Task('Task'))->done();
-        $this->assertTrue($this->getPrivateProperty($task, 'isDone'));
+        $this->assertTrue($task->isDone());
+        $this->assertInstanceOf(\DateTimeImmutable::class, $task->getDoneAt());
     }
 
     public function testUndone()
     {
         $task = (new Task('Task'))->undone();
-        $this->assertFalse($this->getPrivateProperty($task, 'isDone'));
+        $this->assertFalse($task->isDone());
+        $this->assertNull($task->getDoneAt());
+    }
+
+    public function testDelete()
+    {
+        $task = (new Task('Task'))->delete();
+        $this->assertTrue($task->isDeleted());
+        $this->assertInstanceOf(\DateTimeImmutable::class, $task->getDeletedAt());
+    }
+
+    public function testRestore()
+    {
+        $task = (new Task('Task'))->restore();
+        $this->assertFalse($task->isDeleted());
+        $this->assertNull($task->getDeletedAt());
     }
 }
