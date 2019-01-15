c6b1dffaa75ce9bb20eca5d216a299ce24f29552
krlv <eug.krlv@gmail.com>
2019-01-14 23:52:51 -0800

    #31 Create TaskHydrator

--- src/Hydrator/HydratorInterface.php
+++ src/Hydrator/HydratorInterface.php
@@ -6,6 +6,8 @@ namespace Skeleton\Hydrator;
 
 interface HydratorInterface
 {
+    public const DATE_FORMAT = 'Y-m-d H:i:s';
+
     public function hydrate(array $data): object;
 
     public function toArray(object $object): array;
