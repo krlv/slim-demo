e44151e37a2fa63e55aae5ae5893a960328a2c18
krlv <eug.krlv@gmail.com>
2020-08-08 02:52:21 -0700

    #45 Switch to middlewares/payload package

--- config/middleware.php
+++ config/middleware.php
@@ -2,12 +2,11 @@
 
 declare(strict_types=1);
 
-use Middlewares\TrailingSlash;
 use Skeleton\App\Middleware;
 use Slim\App;
 
 return static function (App $app) {
     $app->add(Middleware\LoggerMiddleware::class);
-    $app->addMiddleware(new TrailingSlash(false));
-    $app->addMiddleware(new Middleware\JsonBodyParserMiddleware());
+    $app->addMiddleware(new Middlewares\TrailingSlash(false));
+    $app->addMiddleware(new Middlewares\JsonPayload());
 };
